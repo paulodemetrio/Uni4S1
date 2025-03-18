9ca00a090ea9528081edfddadda1ed6317dd53fd
paulodemetrio <pdemetriocom@gmail.com>
2025-03-18 19:06:47 -0300

    ini

---
+++ src/Exe1.java
@@ -0,0 +1,10 @@
+public class Exe1 {
+    public static void main(String[] args) {
+        int idade = 18;
+        if (idade >= 18) {
+            System.out.println("Maior de idade.");
+        } else {
+            System.out.println("Menor de idade.");
+        }
+    }
+}
