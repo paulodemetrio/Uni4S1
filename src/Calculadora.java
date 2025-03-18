e5c2a7c10c599b9358a57ab345d6cfe6b62d97e8
paulodemetrio <pdemetriocom@gmail.com>
2025-03-18 20:30:17 -0300

    calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,31 +1,33 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
         System.out.println("Digite o primeiro número: ");
         double num1 = scan.nextDouble();
         System.out.println("Digite o segundo número: ");
         double num2 = scan.nextDouble();
         System.out.println("Digite a operação desejada: ");
-        System.out.println("1 - Soma");
-        System.out.println("2 - Subtração");
-        System.out.println("3 - Multiplicação");
-        System.out.println("4 - Divisão");
-        int op = scan.nextInt();
+        System.out.println("'+' - Soma");
+        System.out.println("'-' - Subtração");
+        System.out.println("'*' - Multiplicação");
+        System.out.println("'/' - Divisão");
+        char op = scan.next().charAt(0);
         double result = 0;
-        if (op == 1) {
+        if (op == '+') {
             result = num1 + num2;
-            System.out.println("Resultado: " + result);
-        } else if (op == 2) {
+            System.out.println("Resultado: " + df.format(result));
+        } else if (op == '-') {
             result = num1 - num2;
-            System.out.println("Resultado: " + result);
-        } else if (op == 3) {
+            System.out.println("Resultado: " + df.format(result));
+        } else if (op == '*') {
             result = num1 * num2;
-            System.out.println("Resultado: " + result);
-        } else if (op == 4) {
+            System.out.println("Resultado: " + df.format(result));
+        } else if (op == '/') {
             result = num1 / num2;
-            System.out.println("Resultado: " + result);
+            System.out.println("Resultado: " + df.format(result));
         } else {
             System.out.println("Operação inválida.");
         }
