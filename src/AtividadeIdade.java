3cb7ba80a2d2789ac5b50d9f005b5b8e948ab7c8
paulodemetrio <pdemetriocom@gmail.com>
2025-03-18 19:18:21 -0300

    a

---
+++ src/AtividadeIdade.java
@@ -0,0 +1,12 @@
+public class AtividadeIdade {
+    public static void main(String[] args) throws Exception {
+        int idade = 19;
+        if (idade >= 18) {
+            System.out.print("Adulto");
+        } else if (idade >= 6) {
+            System.out.print("Adolescente");
+        } else {
+            System.out.print("Infantil");
+        }
+    }
+}
